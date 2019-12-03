224458ab15b5fb116905b60868a207b07c49b645
rndpatel@gmail.com <rndpatel@gmail.com>
2019-12-03 13:30:29 -0800

    GET, POST, PUT, working

--- data/posts/postsRouter.js
+++ data/posts/postsRouter.js
@@ -4,6 +4,24 @@ const db = require('../db');
 const router = express.Router();
 
 
+function getPost(id, res){
+  return(
+    db.findById(id)
+      .then(([post]) => {
+        console.log(post);
+        if(post) {
+          res
+            .status(201)
+            .json(post)
+        } else {
+          res
+            .status(404)
+            .json({message: "The post with the specified ID does not exist."})
+        }
+      })
+  )
+}
+
 // **** /api/posts ****
 
 router.get('/', (req, res) => {
@@ -30,40 +48,25 @@ router.post('/', (req, res) => {
   }
   db.insert({title, contents})
     .then(({id}) => {
-      db.findById(id)
-        .then(([post]) => {
-          console.log(post);
-          res
-            .status(201)
-            .json(post)
-        })
+      getPost(id, res)
     })
     .catch(error => {
       console.log(error);
       res
         .status(500)
-        .json({error: "There was an error while savint he post to the database."})
+        .json({error: "There was an error while saving he post to the database."})
     })
 });
 
 // **** /api/posts ****
 
 
+
+// **** /api/posts/:id ****
+
 router.get(`/:id`, (req, res) => {
   const { id } = req.params;
-  db.findById(id)
-    .then(([post]) => {
-      console.log(post);
-      if(post) {
-        res
-          .status(200)
-          .json(post)
-      } else {
-        res
-          .status(404)
-          .json({message: "The post with the specified ID does not exist."})
-      }
-    })
+    getPost(id)
     .catch(error => {
       console.log(error);
       res
@@ -71,5 +74,38 @@ router.get(`/:id`, (req, res) => {
         .json({error: "The post information could not be retrieved."})
     })
 });
+
+router.put(`/:id`, (req, res) => {
+  const { id } = req.params;
+  const {title, contents} = req.body;
+  if (!title && !contents) {
+    return res
+      .status(400)
+      .json({ errorMessage: "Please provide title and contents for the post." })
+  }
+  db.update(id,{title, contents})
+    .then(updated => {
+      if(updated) {
+      console.log(updated);
+      getPost(id, res)
+    } else {
+      res
+        .status(404)
+        .json({message: "The post with the specified ID does not exist"})
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res
+        .status(500)
+        .json({ error: "The post information could not be modified." })
+    })
+
+});
+
+// **** /api/posts/:id ****
+
+
+
 
 module.exports = router;
