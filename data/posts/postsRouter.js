451a049f26e9bae601ed492e6c7fd46073ca0c47
rndpatel@gmail.com <rndpatel@gmail.com>
2019-12-03 12:31:01 -0800

    GET by ID working

--- data/posts/postsRouter.js
+++ data/posts/postsRouter.js
@@ -14,8 +14,31 @@ router.get('/', (req, res) => {
       console.log(error);
       res
         .status(500)
-        .json("The posts information could not be retrieved.")
+        .json({error: "The posts information could not be retrieved."})
     });
+});
+
+router.get(`/:id`, (req, res) => {
+  const { id } = req.params;
+  db.findById(id)
+    .then(post => {
+      console.log(post);
+      if(post.length) {
+        res
+          .status(200)
+          .json(post)
+      } else {
+        res
+          .status(404)
+          .json({message: "The post with the specified ID does not exist."})
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res
+        .status(500)
+        .json({error: "The post information could not be retrieved."})
+    })
 });
 
 module.exports = router;
